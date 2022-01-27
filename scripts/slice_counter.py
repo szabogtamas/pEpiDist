f9f7f8f56541d02e65320d31c04142c6d9392746
szabogtamas <szabogtamas@gmail.com>
2022-01-27 16:18:26 +0100

    Added fasta parsing

--- scripts/slice_counter.py
+++ scripts/slice_counter.py
@@ -8,6 +8,15 @@ parser.add_argument("proteom_file")
 parser.add_argument("output_dir")
 args = parser.parse_args()
 
+def parse_fasta_proteome(fasta_file):
+    with open(fasta_file, "r") as f:
+        fasta_seqs = f.read().split(">")
+        protein_seqs = list()
+        for s in fasta_seqs[1:]:
+            name, seq = s.split("\n", 1)
+            protein_seqs.append((name.split("|")[1], seq.replace("\n", "").replace(" ", "")))
+    return protein_seqs
+
 def count_peptides_of_seq(protein, protein_seq, window=9, epitopes=None):
     if epitopes is None:
         epitopes = defaultdict(list)
@@ -16,16 +25,16 @@ def count_peptides_of_seq(protein, protein_seq, window=9, epitopes=None):
     return epitopes
  
 def count_peptides_in_collection(proteins):
-  epi_map = defaultdict(list)
-  for name, seq in proteins:
-    epi_map = count_peptides_of_seq(name, seq, epitopes=epi_map)
-  return epi_map
+      epi_map = defaultdict(list)
+      for name, seq in proteins:
+        epi_map = count_peptides_of_seq(name, seq, epitopes=epi_map)
+      return epi_map
 
 def main(proteom_file, output_dir):
-  proteins = parse_fasta_proteome(proteom_file)
-  epitope_numbers = count_peptides_in_collection(proteins)
-  return
+      proteins = parse_fasta_proteome(proteom_file)
+      epitope_numbers = count_peptides_in_collection(proteins)
+      return
 
 
 if __name__ == "__main__":
-    main(args.proteom_file, args.output_dir)
+      main(args.proteom_file, args.output_dir)
