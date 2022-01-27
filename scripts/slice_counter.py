ab931197ce06c41909f00abeab101211b4b2601d
szabogtamas <szabogtamas@gmail.com>
2022-01-27 16:05:13 +0100

    Updated multiseq counter function

--- scripts/slice_counter.py
+++ scripts/slice_counter.py
@@ -9,11 +9,14 @@ parser.add_argument("output_dir")
 args = parser.parse_args()
 
 def count_peptides_of_seq(protein_seq, window=9):
-    epitopes = [protein_seq[i:i+window] for i in range(len(protein_seq)-window+1)]
-    return Counter(epitopes)
+   epitopes = [protein_seq[i:i+window] for i in range(len(protein_seq)-window+1)]
+   return Counter(epitopes)
  
 def count_peptides_in_collection(proteins):
-  return
+  epi_map = dict()
+  for name, seq in proteins:
+    epi_map = count_peptides_of_seq(name, seq, epi_map)
+  return epi_map
 
 def main(proteom_file, output_dir):
   proteins = parse_fasta_proteome(proteom_file)
