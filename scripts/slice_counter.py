868030233f77be40b3661edd56a66f1e5f168c39
szabogtamas <szabogtamas@gmail.com>
2022-01-28 19:41:42 +0100

    Added option to control peptide window

--- scripts/slice_counter.py
+++ scripts/slice_counter.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 parser = argparse.ArgumentParser()
 parser.add_argument("proteome_file")
 parser.add_argument("output_dir")
+parser.add_argument("window_size", default=8)
 args = parser.parse_args()
 
 def parse_fasta_proteome(fasta_file):
@@ -24,17 +25,17 @@ def count_peptides_of_seq(protein, protein_seq, window=9, epitopes=None):
         epitopes[protein_seq[i:i+window]].append(str(i) + "_" + protein)
     return epitopes
  
-def count_peptides_in_collection(proteins):
+def count_peptides_in_collection(proteins, window_size):
       epi_map = defaultdict(list)
       for name, seq in proteins:
-        epi_map = count_peptides_of_seq(name, seq, epitopes=epi_map)
+        epi_map = count_peptides_of_seq(name, seq, window_size, epitopes=epi_map)
       return epi_map
 
-def main(proteome_file, output_dir):
+def main(proteome_file, output_dir, window_size):
       proteins = parse_fasta_proteome(proteome_file)
-      epitope_numbers = count_peptides_in_collection(proteins)
+      epitope_numbers = count_peptides_in_collection(proteins, window_size)
       return
 
 
 if __name__ == "__main__":
-      main(args.proteome_file, args.output_dir)
+      main(args.proteome_file, args.output_dir, args.window_size)
