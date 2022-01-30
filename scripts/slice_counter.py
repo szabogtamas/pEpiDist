ebd6deb6a5af327fe5ea79768f9de2799bea4728
szabogtamas <szabogtamas@gmail.com>
2022-01-30 14:37:51 +0100

    Added docstrings

--- scripts/slice_counter.py
+++ scripts/slice_counter.py
@@ -13,22 +13,25 @@ def main():
     return
 
 def create_peptide_db(proteome_file, output_dir, window_size):
+    """The main coordinating function that processes a fasta proteome and returns shared peptides."""
     proteins = parse_fasta_proteome(proteome_file)
     epitope_numbers = count_peptides_in_collection(proteins, window_size)
     overlap_list = summarise_pepinet(epitope_numbers)
     return overlap_list
 
 def parse_fasta_proteome(fasta_file):
+    """Yet another FASTA parser to read input sequences."""
     with open(fasta_file, "r") as f:
         fasta_seqs = f.read().split("\n>")
         protein_seqs = fasta_seqs[0].split("\n", 1)
         protein_seqs = [(protein_seqs[0].replace(">", "").split("|")[0], protein_seqs[1].replace("\n", "").replace(" ", ""))]
         for s in fasta_seqs[1:]:
-          name, seq = s.split("\n", 1)
-          protein_seqs.append((name.split("|")[0], seq.replace("\n", "").replace(" ", "")))
+            name, seq = s.split("\n", 1)
+            protein_seqs.append((name.split("|")[0], seq.replace("\n", "").replace(" ", "")))
     return protein_seqs
 
 def count_peptides_of_seq(protein, protein_seq, window=9, epitopes=None):
+    """The workhorse function that actually counts peptides in a given sequence."""
     if epitopes is None:
         epitopes = defaultdict(list)
     for i in range(len(protein_seq)-window+1):
@@ -36,12 +39,14 @@ def count_peptides_of_seq(protein, protein_seq, window=9, epitopes=None):
     return epitopes
  
 def count_peptides_in_collection(proteins, window_size):
+    """Issues the peptide counting function for all sequences."""
     epi_map = defaultdict(list)
     for name, seq in proteins:
         epi_map = count_peptides_of_seq(name, seq, window_size, epitopes=epi_map)
     return epi_map
 
 def summarise_pepinet(d):
+    """Sums up the results and sorts by the number of proteins the peptide overlaps."""
     summary_stats = [(k, len(v), ";".join(v)) for k, v in d.items()]
     summary_stats.sort(key=lambda x: x[1], reverse=True)
     return summary_stats
