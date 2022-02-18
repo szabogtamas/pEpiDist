e7fbab15373abf460a6c2abe0bf2aee8a988f52f
szabogtamas <szabogtamas@gmail.com>
2022-02-18 15:45:13 +0100

    Reusing fasta parser

--- scripts/query_mimicri.py
+++ scripts/query_mimicri.py
@@ -5,11 +5,12 @@ def main():
     parser.add_argument("input_file")
     parser.add_argument("output_file", nargs="?", default="kidera_distances.tsv")
     args = parser.parse_args()
-    native_result = query_mimicry_peptides(args.input_file)
+    query_proteome = parse_fasta_proteome(args.input_file)
+    native_result = query_mimicry_peptides(query_proteome)
     with open(args.output_file, "w") as f:
         f.write("\n".join([a + "\t" + str(b) + "\t" + c for a, b, c in native_result]))
     return
-  
+
 def query_mimicry_peptides(q):
     """Calculates similarity of epitope peptides in query file to epitopes in reference proteome."""
     distance = []
